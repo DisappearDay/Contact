c2f09a3f308dd643f8e44a050b8bf87d24621828
DisappearDay <117563652+DisappearDay@users.noreply.github.com>
2023-04-18 11:28:56 +0800

    Update contact.h

--- contact.h
+++ contact.h
@@ -10,28 +10,56 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 
-
-
+//通讯录用户信息的结构体
 struct PeoInfo
 {
-	char name[NAME_MAX];
-	int age;
-	char sex[SEX_MAX];
-	char tele[TELE_MAX];
-	char adress[ADRESS_MAX];
-	
+	char name[NAME_MAX];  //名字
+	int age;              //年龄
+	char sex[SEX_MAX];    //性别 
+	char tele[TELE_MAX];  //电话
+	char adress[ADRESS_MAX];  //住址
+
 };
 
+//通讯录
 struct Contact
 {
-	struct PeoInfo data[MAX];
-	int size;
+	struct PeoInfo data[MAX];//一个通讯录数组
+	int size;    //计算通讯录里面用户的个数
+};
+
+enum Option
+{
+	Exit,
+    Add,
+    Del,
+	Search,
+	Modify,
+	Show,
+	Sort
 };
 
 
-
+//初始化通讯录
 void init_contact(struct Contact* str);
-void Add_Contract(struct Contact* str);
-void Show_Contact(struct Contact* str);
+
+//增加一个通讯录的用户信息
+void Add_Contact(struct Contact* str);
+
+//展示通讯录所有用户信息
+void Show_Contact(const struct Contact* str);
+
+//删除一个通讯录的用户信息
+void Del_Contact(struct Contact* str);
+
+//查询一个用户信息
+void Search_Contact(const struct Contact* str);
+
+//修改通讯录的信息
+void Modify_Contact(struct Contact* str);
+
+//对改通讯录进行排序
+void Sort_Contact(const struct Contact* str);
